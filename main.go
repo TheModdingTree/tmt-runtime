c3f4f03a85fde71caac8f865996b546ab0acf81b
braeden <flamemasternxf@gmail.com>
2026-03-28 15:06:19 -0400

    feat: The actual server runner

--- main.go
+++ main.go
@@ -1,12 +1,89 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"flag"
 	"fmt"
+	"net"
+	"net/http"
+	"os"
 	"os/exec"
+	"os/signal"
+	"path/filepath"
 	"runtime"
+	"syscall"
+	"time"
 )
 
-func main() {}
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "runtime error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	dirFlag := flag.String("dir", ".", "The directory which contains your mod files")
+	flag.Parse()
+	if flag.NArg() != 0 {
+		return fmt.Errorf("unexpected arguments: %v", flag.Args())
+	}
+
+	dir, err := filepath.Abs(*dirFlag)
+	if err != nil {
+		return fmt.Errorf("invalid project directory: %w", err)
+	}
+	info, err := os.Stat(dir)
+	if err != nil {
+		return fmt.Errorf("cannot access project directory %q: %w", dir, err)
+	}
+	if !info.IsDir() {
+		return fmt.Errorf("project path is not a directory: %q", dir)
+	}
+
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		return err
+	}
+	defer listener.Close()
+
+	port := listener.Addr().(*net.TCPAddr).Port
+	url := fmt.Sprintf("http://127.0.0.1:%d/", port)
+
+	server := &http.Server{Handler: http.FileServer(http.Dir(dir))}
+	serveErr := make(chan error, 1)
+
+	go func() {
+		err := server.Serve(listener)
+		if errors.Is(err, http.ErrServerClosed) {
+			err = nil
+		}
+		serveErr <- err
+	}()
+
+	fmt.Printf("Serving files from %s\nURL: %s\n", dir, url)
+
+	if err := openBrowser(url); err != nil {
+		fmt.Printf("Warning: failed to open browser automatically: %v\n", err)
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	select {
+	case err := <-serveErr:
+		return err
+	case <-ctx.Done():
+	}
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		return err
+	}
+	return <-serveErr
+}
 
 func openBrowser(url string) error {
 	switch runtime.GOOS {
