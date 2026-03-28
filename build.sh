f609e0749f6238c5d268edf6a68e64211c7fe34d
braeden <flamemasternxf@gmail.com>
2026-03-28 14:35:31 -0400

    feat: build.sh

---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/sh
+
+DIST_DIR="./dist"
+mkdir -p "$DIST_DIR"
+echo "Building runtime binaries into $DIST_DIR"
+
+build_target() {
+	goos="$1"
+	goarch="$2"
+	ext=""
+	[ "$goos" = "windows" ] && ext=".exe"
+
+	output="$DIST_DIR/tmt-runtime-${goos}-${goarch}${ext}"
+	echo "  - $goos/$goarch -> $(basename "$output")"
+
+	CGO_ENABLED=0 GOOS="$goos" GOARCH="$goarch" go build -trimpath -ldflags="-s -w" -o "$output" .
+}
+
+build_target linux amd64
+build_target linux arm64
+build_target darwin amd64
+build_target darwin arm64
+build_target windows amd64
+build_target windows arm64
+
+echo "Done!"
+echo ",,, ,,,*/#%%%%%&&&&&&&&%%#/,,..*                  "
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#/.*.....      "
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@(,.     ."
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%#///&. . ."
+echo "@@@@@@@@@@@@&&,#**//*/@@@@@@@@@@@@@@@@@@@@,&@@@@//"
+echo "@@&&&%###((((*,(%@@@@@@@@@@@@@@@@@@@@@@@% @@@@@@@ "
+echo "((((((((((((./((#%@@@@@@@@@@@@@@@@@&%%&@@.@@@@@@@ "
+echo "(((((((((((./(((((#&%*    %@@@@@@@&, ,&&&&@@@@@@@ "
+echo "(((((((((((./(((((((((#%%%####@%*&&&&&&&&@@@@@@@@ "
+echo "((((((((((((./((((((((((((((((#%%%&&&%&@@@@@@@@@@ "
+echo "((((((((((((((##############%#%%&&@@@@@@@@@@@@@@@#"
+echo "#%%&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@/  "
+echo "  .(%&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%/       "
